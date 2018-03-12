e64b82ff9c5003585c052c7b23f8cad6d9fe62dd
Tom-J-Barrett <motb141@gmail.com>
2018-03-12 19:09:38 +0000

    okhttp

--- app/src/main/java/com/example/tom13/fooddemo/backgroundProcesses/UploadImage.java
+++ app/src/main/java/com/example/tom13/fooddemo/backgroundProcesses/UploadImage.java
@@ -1,12 +1,15 @@
 package com.example.tom13.fooddemo.backgroundProcesses;
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.util.Log;
 
 import com.example.tom13.fooddemo.host.Host;
 import com.example.tom13.fooddemo.presenters.CaptureImagePresenter;
 import com.example.tom13.fooddemo.requests.RequestFactory;
 
+
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -26,7 +29,16 @@ import java.io.File;
 import java.io.IOException;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
 
 import static org.apache.http.entity.mime.HttpMultipartMode.BROWSER_COMPATIBLE;
 
@@ -36,6 +48,7 @@ import static org.apache.http.entity.mime.HttpMultipartMode.BROWSER_COMPATIBLE;
 
 public class UploadImage extends AsyncTask<Void, Void, String> {
 
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
     private Host host;
     private String image;
     private CaptureImagePresenter captureImagePresenter;
@@ -54,19 +67,42 @@ public class UploadImage extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... params) {
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost(host.getUrl());
+//        HttpClient httpclient = new DefaultHttpClient();
+//        HttpPost httppost = new HttpPost(host.getUrl());
+//
+//        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
+//        multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+//        multipartEntityBuilder.addBinaryBody("file", file, ContentType.DEFAULT_BINARY, "file");
+//
+//        HttpEntity httpEntity = multipartEntityBuilder.build();
+//        httppost.setEntity(httpEntity);
+//        try {
+//            Log.v("message", host.getUrl());
+//            HttpResponse httpResponse = httpclient.execute(httppost);
+//            Log.v("message", String.valueOf(httpResponse.getStatusLine().getStatusCode()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        MultipartEntityBuilder multipartEntityBuilder = MultipartEntityBuilder.create();
-        multipartEntityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-        multipartEntityBuilder.addBinaryBody("file", file, ContentType.DEFAULT_BINARY, "file");
+        OkHttpClient client = new OkHttpClient();
 
-        HttpEntity httpEntity = multipartEntityBuilder.build();
-        httppost.setEntity(httpEntity);
+//        RequestBody requestBody = new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("file", "image.jpg", RequestBody.create(MediaType.parse("image/jpg"), file))
+//                .build();
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", image)
+                .build();
+
+        Request request = new Request.Builder().url(host.getUrl())
+                .post(requestBody).build();
+
+        Response response = null;
         try {
-            Log.v("message", host.getUrl());
-            HttpResponse httpResponse = httpclient.execute(httppost);
-            Log.v("message", String.valueOf(httpResponse.getStatusLine().getStatusCode()));
+            response = client.newCall(request).execute();
+            Log.v("request", response.body().string());
+            response.body().close();
         } catch (IOException e) {
             e.printStackTrace();
         }
